1cfe0a0766e0f1951d9e062abb4157410c4cc5a9
angelusMZ19 <115651373+angelusMZ19@users.noreply.github.com>
2023-11-04 18:56:00 +0100

    actualizacion

--- Desarrollo Entorno Cliente/Octubre_/js/OCT05_2023.js
+++ Desarrollo Entorno Cliente/Octubre_/js/OCT05_2023.js
@@ -1,7 +1,29 @@
 //funciones de la sesion 5 del html
+//conversion de temperatura
+function convertirTemperatura() {
+    let tipo = prompt("Escribe 'f' para convertir de Fahrenheit a Celsius o 'c' para convertir de Celsius a Fahrenheit");        
+    let temp = parseFloat(prompt("Introduce la temp: "));
+    let conversion;
+
+    switch (tipo) {
+        case 'f':
+            conversion = (temp - 32) * 5/9;
+            console.log(temp + " Fahrenheit equivale a " + conversion + " Celsius");
+            alert(temp + " Fahrenheit equivale a " + conversion + " Celsius");
+            break;
+        case 'c':
+            conversion = (temp * 9/5) + 32;
+            console.log(temp + " Celsius equivale a " + conversion + " Fahrenheit");
+            alert(temp + " Celsius equivale a " + conversion + " Fahrenheit");
+            break;
+        default:
+            console.log("Entrada inválida. Por favor, escribe 'f' o 'c'.");
+            break;
+    }
+}
+
 //pedir un numero al usuario i convertirlo a binario, octal, hexanumero
 //utilizando bucles division y residuo
-
 function escribe() {
     let numero = parseInt(prompt("Ingresa el numero"));
     convertirBinario(numero);
--- Desarrollo Entorno Cliente/Octubre_/js/OCT16_2023.js
+++ Desarrollo Entorno Cliente/Octubre_/js/OCT16_2023.js
@@ -23,6 +23,33 @@ function creaTablas(filas, columnas){
 }
 
 /*no olvidarse de hacer la de la tabla de multiplicar */
+function tablaMultiplicar() {
+    let filas = parseInt(prompt("Ingresa el número de filas"));
+    let columnas = parseInt(prompt("Ingresa el número de columnas"));
+
+    let tabla = document.createElement("table");
+    tabla.style.borderCollapse = "collapse";
+    tabla.style.margin = "20px";
+
+    for (let i = 1; i <= filas; i++) {
+        let fila = document.createElement("tr");
+        tabla.appendChild(fila);
+        for (let j = 1; j <= columnas; j++) {
+            let celda = document.createElement("td");
+            celda.style.border = "1px solid black";
+            celda.style.padding = "10px";
+            celda.style.textAlign = "center";
+            celda.style.fontWeight = "bold";
+            fila.appendChild(celda);
+
+            let contenido = document.createTextNode(i * j);
+            celda.appendChild(contenido);
+        }
+    }
+
+    document.body.appendChild(tabla);
+}
+
 
 function dibujarPiramide() {
     let col = parseInt(prompt("Base de la pirámide?"));
